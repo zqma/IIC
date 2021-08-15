66e7cf20b7a168e2ccb3e3ea558e338ca57cc535
Zhiqiang Ma <zma@Zhiqiangs-MacBook-Pro.local>
2021-08-15 16:32:41 -0400

    adjust code to run

---
+++ proj/archs/cluster/__init__.py
@@ -0,0 +1,5 @@
+from proj.archs.cluster.baselines import *
+from proj.archs.cluster.net5g import *
+from proj.archs.cluster.net5g_two_head import *
+from proj.archs.cluster.net6c import *
+from proj.archs.cluster.net6c_two_head import *
